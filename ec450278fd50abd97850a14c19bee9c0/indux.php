92add974cf6ff36093e98295abc16102a825a42a
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2021-12-02 14:51:10 +0000

    Update indux.php

--- ec450278fd50abd97850a14c19bee9c0/indux.php
+++ ec450278fd50abd97850a14c19bee9c0/indux.php
@@ -5,7 +5,7 @@
         
         Grabber URL Payload logger
 
-        Part of the YoutubeIPGrabber; https://github.com/mrbid
+        Part of the YoutubeIPGrabber; https://github.com/mrbid/Youtube-IP-Grabber
     */
     $r = str_replace('&ua=', "&ua=".date("Y-m-d H:i:s: "), $_SERVER['QUERY_STRING']);
     file_put_contents("log.txt", $r . "\n", FILE_APPEND | LOCK_EX);
